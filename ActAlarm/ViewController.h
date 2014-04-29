7a21acf3cac8e7304b0053507354d146644522a4
externalight <externalight@gmail.com>
2014-04-29 23:20:31 +0900

    Initial Commit

---
+++ ActAlarm/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ActAlarm
+//
+//  Created by Masakazu Aoki on 2014/04/29.
+//  Copyright (c) 2014年 externalight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ActAlarm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ActAlarm
+//
+//  Created by Masakazu Aoki on 2014/04/29.
+//  Copyright (c) 2014年 externalight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
